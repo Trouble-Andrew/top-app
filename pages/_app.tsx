006ff810ded352ef9a1795f1e807522627f57cab
Andrey Terekhov <satyr663@gmail.com>
2022-08-11 14:09:00 +0300

    feat: add typescript linter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import '../styles/globals.css';
+import { AppProps } from 'next/dist/shared/lib/router/router';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
